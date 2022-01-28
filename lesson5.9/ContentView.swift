1494210235d6cdd5caad764a47afc676506df8d5
Javlonbek <javlonbekalhamdulillah@gmail.com>
2022-01-28 18:38:18 +0500

    Initial Commit

---
+++ lesson5.9/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  lesson5.9
+//
+//  Created by Javlonbek on 28/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ lesson5.9/lesson5_9App.swift
@@ -0,0 +1,17 @@
+//
+//  lesson5_9App.swift
+//  lesson5.9
+//
+//  Created by Javlonbek on 28/01/22.
+//
+
+import SwiftUI
+
+@main
+struct lesson5_9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
